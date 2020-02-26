1f145a1529cb49ddcc56576f2ea8cdf6ce6ab5b6
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-02-26 14:27:23 +0900

    storeの作成

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
+
+// Storeの作成
+let store = createStore(rootReducer);
