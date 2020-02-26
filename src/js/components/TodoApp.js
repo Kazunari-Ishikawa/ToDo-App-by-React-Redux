82409dfcbd5ca6421cc0fdef8277f0cd215027be
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-02-26 16:06:37 +0900

    コンポーネント準備

--- src/js/components/TodoApp.js
+++ src/js/components/TodoApp.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import TodoCreater from '../components/TodoCreator';
+import Search from '../components/Search';
+import VisibleTodoList from '../containers/VisibleTodoList';
 
 export default class todoApp extends React.Component {
   render() {
     return (
-      <div className="sample">
-        テスト
+      <div className="l-app">
+        <TodoCreater />
+        <Search />
+        <VisibleTodoList />
       </div>
     );
   }
