dded9e7ba246f2aa84e32506284c650162d5b41a
kazunari-ishikawa <zunari.computercity@gmail.com>
2020-02-27 16:07:42 +0900

    subtextのランダム表示追加

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -53717,24 +53717,45 @@ var Title = function (_React$Component) {
   function Title() {
     _classCallCheck(this, Title);
 
-    return _possibleConstructorReturn(this, (Title.__proto__ || Object.getPrototypeOf(Title)).apply(this, arguments));
+    var _this = _possibleConstructorReturn(this, (Title.__proto__ || Object.getPrototypeOf(Title)).call(this));
+
+    _this.state = {
+      messages: ['There is always light behind the clouds.', 'Change before you have to.', 'If you can dream it, you can do it.', 'Love the life you live. Live the life you love.', 'Without haste, but without rest.', 'Stay hungry. Stay foolish.', 'Failure is inevitable. Success is elusive.', 'I’m happy to be alive, I’m happy to be who I am.', 'Genius is 1 percent inspiration and 99 percent perspiration.', 'As you think, so shall you become.'],
+      author: ['Louisa May Alcott', 'Jack Welch', 'Walt Disney', 'Bob Marley', 'Johann Wolfgang von Goethe', 'Steve Jobs', 'Steven Spielberg', 'Michael Jackson', 'Thomas Edison', 'Bruce Lee']
+    };
+    return _this;
   }
 
   _createClass(Title, [{
-    key: "render",
+    key: 'randomMsg',
+    value: function randomMsg() {
+      var random = Math.floor(Math.random() * 10);
+      return [{ msg: this.state.messages[random], auth: this.state.author[random] }];
+    }
+  }, {
+    key: 'render',
     value: function render() {
+      var subText = this.randomMsg();
+      console.log(subText);
       return _react2.default.createElement(
-        "div",
-        { className: "c-title" },
+        'div',
+        { className: 'c-title' },
         _react2.default.createElement(
-          "h1",
-          { className: "c-title__main" },
-          "TODO LIST"
+          'h1',
+          { className: 'c-title__main' },
+          'TODO LIST'
         ),
         _react2.default.createElement(
-          "h2",
-          { className: "c-title__sub" },
-          "If you can dream it, you can do it."
+          'h2',
+          { className: 'c-title__sub' },
+          subText[0].msg
+        ),
+        _react2.default.createElement(
+          'h5',
+          { className: 'c-title__author' },
+          '- ',
+          subText[0].auth,
+          ' -'
         )
       );
     }
--- src/js/components/Title.js
+++ src/js/components/Title.js
@@ -1,11 +1,45 @@
 import React from 'react';
 
 class Title extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: ['There is always light behind the clouds.',
+        'Change before you have to.',
+        'If you can dream it, you can do it.',
+        'Love the life you live. Live the life you love.',
+        'Without haste, but without rest.',
+        'Stay hungry. Stay foolish.',
+        'Failure is inevitable. Success is elusive.',
+        'I’m happy to be alive, I’m happy to be who I am.',
+        'Genius is 1 percent inspiration and 99 percent perspiration.',
+        'As you think, so shall you become.'
+      ],
+      author: ['Louisa May Alcott',
+        'Jack Welch',
+        'Walt Disney',
+        'Bob Marley',
+        'Johann Wolfgang von Goethe',
+        'Steve Jobs',
+        'Steven Spielberg',
+        'Michael Jackson',
+        'Thomas Edison',
+        'Bruce Lee']
+    };
+  }
+  randomMsg() {
+    const random = Math.floor(Math.random() * 10);
+    return [
+      { msg: this.state.messages[random], auth: this.state.author[random] }
+    ];
+  }
   render() {
+    const subText = this.randomMsg();
     return (
       <div className="c-title">
         <h1 className="c-title__main">TODO LIST</h1>
-        <h2 className="c-title__sub">If you can dream it, you can do it.</h2>
+        <h2 className="c-title__sub">{subText[0].msg}</h2>
+        <h5 className="c-title__author">- {subText[0].auth} -</h5>
       </div>
     );
   }
